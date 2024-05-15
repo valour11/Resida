848ef191836c21ef585815f4b6c75e06bebc1d97
Gideon Abel <gideonabel87@gmail.com>
2024-05-15 13:56:09 +0100

    header and footer structure created

---
+++ src/shared/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './FooterStyle.css'
+
+const Footer = () => {
+  return (
+    <div className='main-footer'>
+      <h1>Footer here</h1>
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/shared/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './HeaderStyle.css'
+
+const Header = () => {
+  return (
+    <div className='main-header'>
+      <h1>Header here</h1>
+    </div>
+  )
+}
+
+export default Header
