2b86386fe17b0ffdfa35de7599400236ae015d89
Gideon Abel <gideonabel87@gmail.com>
2024-06-13 14:24:52 +0100

    added git workflows

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  // base: "/home",
   build: {
     rollupOptions: {
       external: ['react-icons/md'], // Externalize react-icons/md
