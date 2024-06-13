4da125b8d5c87427ca67bbba4f4e17b0725c4e00
Gideon Abel <gideonabel87@gmail.com>
2024-06-13 13:53:53 +0100

    added vercel.json

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,4 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
